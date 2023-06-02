c807426c673ac8721e9498e31901988333b4a4bf
Edullidan <128309694+Edullidan@users.noreply.github.com>
2023-06-02 12:42:16 +0400

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {Users} from './interface'
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  timeout: 1000,
+  headers: {'X-Custom-Header': 'foobar'}
+});
 
 function App() {
+  const [users, setUsers] = useState <Users[] | null>(null);
+
+
+const createUser = async () => {
+  const response = await axios.put('https://fakestoreapi.com/products/7', {
+    title: "test product",
+    price: "13.5",
+    description :"lorem ipsum set",
+    image: "https://i.pravatar.cc",
+    category: "electronic"
+  })
+  console.log("Fake postis responce", response)
+}
+
+
+  useEffect(() => {
+         const requestUser = async () => {
+          const response = await axiosInstance.get("/users")
+          const data = await response.data
+
+        console.log(data)
+          setUsers(data);
+         }
+         requestUser();
+  }, []);
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello World</h1>
+      <ul> {users && users.map((user) => <li key={user.id}>{user.name}</li>)}</ul>
+
+      <button onClick={createUser}>Make post request</button>
     </div>
   );
 }
